49b279c8a17b1cede8ea74dae6b528dd34176147
Traian Alexandru <victor_traian@yahoo.com>
2021-06-12 13:30:04 -0400

    add tootip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import MapChart from "./MapChart";
+import React, { useState } from 'react';
+import MapChart from './MapChart';
+import ReactTooltip from 'react-tooltip';
 
 function App() {
+  const [content, setContent] = useState('');
+
   return (
     <div>
-      <MapChart />
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
     </div>
   );
 }
--- src/MapChart.js
+++ src/MapChart.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { csv } from "d3-fetch";
-import { scaleLinear } from "d3-scale";
+import React, { useEffect, useState } from 'react';
+import { csv } from 'd3-fetch';
+import { scaleLinear } from 'd3-scale';
 import {
   ComposableMap,
   Geographies,
   Geography,
   Sphere,
   Graticule,
-} from "react-simple-maps";
+} from 'react-simple-maps';
 
 const geoUrl =
-  "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
 
-const MapChart = () => {
+const MapChart = ({ setTooltipContent }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -24,19 +24,21 @@ const MapChart = () => {
   const colorScale = scaleLinear()
     .domain(data.map((d) => d.co2))
     .range([
-      "#ffedea",
-      "#ffcec5",
-      "#ffad9f",
-      "#ff8a75",
-      "#ff5533",
-      "#e2492d",
-      "#be3d26",
-      "#9a311f",
-      "#782618",
+      '#ffedea',
+      '#ffcec5',
+      '#ffad9f',
+      '#ff8a75',
+      '#ff5533',
+      '#e2492d',
+      '#be3d26',
+      '#9a311f',
+      '#782618',
     ]);
 
   return (
     <ComposableMap
+      data-tip=""
+      data-type="info"
       projectionConfig={{
         rotate: [-10, 0, 0],
         scale: 130,
@@ -44,39 +46,47 @@ const MapChart = () => {
     >
       <Sphere stroke="#E4E5E6" strokeWidth={0.5} />
       <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-        {data.length > 0 && (
-          <Geographies geography={geoUrl}>
-            {({ geographies }) =>
-              geographies.map((geo) => {
-                const d = data.find(
-                  (s) =>
-                    s.iso_code === geo.properties.ISO_A3 && s.year === "1940"
-                );
+      {data.length > 0 && (
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => {
+              const d = data.find(
+                (s) =>
+                  s.iso_code === geo.properties.ISO_A3 && s.year === '1940',
+              );
 
-                return (
-                  <Geography
-                    key={geo.rsmKey}
-                    geography={geo}
-                    style={{
-                      default: {
-                        fill: d ? colorScale(d.co2) : "#F5F4F6",
-                        outline: "none"
-                      },
-                      hover: {
-                        fill: "#F53",
-                        outline: "none"
-                      },
-                      pressed: {
-                        fill: "#E42",
-                        outline: "none"
-                      }
-                    }}
-                  />
-                );
-              })
-            }
-          </Geographies>
-        )}
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() =>
+                    d
+                      ? setTooltipContent(
+                          `${d?.country} ${d?.year} CO2 Emissions ${d?.co2}`,
+                        )
+                      : ''
+                  }
+                  onMouseLeave={() => setTooltipContent('')}
+                  style={{
+                    default: {
+                      fill: d ? colorScale(d.co2) : '#F5F4F6',
+                      outline: 'none',
+                    },
+                    hover: {
+                      fill: '#F53',
+                      outline: 'none',
+                    },
+                    pressed: {
+                      fill: '#E42',
+                      outline: 'none',
+                    },
+                  }}
+                />
+              );
+            })
+          }
+        </Geographies>
+      )}
     </ComposableMap>
   );
 };
